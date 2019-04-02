b156ad6417fa732668d04401338d9aacb74ebeab
github username <github email address>
2019-04-02 12:16:58 +0100

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,29 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+class DelayedButton extends React.Component {
+
+  handleClick = (event) => {
+    event.persist()
+    setTimeout(() => this.props.onDelayedClick(event), this.props.delay);
+    // console.log(event.clientX, event)
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>delaybutton</button>
+      </div>
+    )
+  }
+}
+
+export default DelayedButton
+
+// In the components/DelayedButton.js file, create a DelayedButton React component
+//
+// This component takes two props: onDelayedClick (a function), and delay (a number).
+//
+// Create a button that, when clicked, will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
+//
+// If successful, the event will be logged to the console once the timeout has finished.
